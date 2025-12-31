c69f26c481e73517c7185074468a4b0ce53b28a0
Somesh Joshi <joshi.somesh1996@gmail.com>
2025-12-31 22:46:44 +0530

    cicd run 11

--- flask_app/app.py
+++ flask_app/app.py
@@ -86,6 +86,7 @@ os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 dagshub_url = "https://dagshub.com"
 repo_owner = "joshi.somesh1996"
 repo_name = "mlops_capstone_end_to_end"
+mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 # -------------------------------------------------------------------------------------
 
 
