c5fadb6e2ee565af827b5bb1887019f91652f8b3
Somesh Joshi <joshi.somesh1996@gmail.com>
2025-12-31 23:16:31 +0530

    cicd run 12

--- src/model/model_evaluation.py
+++ src/model/model_evaluation.py
@@ -108,7 +108,7 @@ def save_model_info(run_id: str, model_path: str, file_path: str) -> None:
         raise
 
 def main():
-    mlflow.set_experiment("my-dvc-pipeline-v4")
+    mlflow.set_experiment("my-dvc-pipeline-v5")
     with mlflow.start_run() as run:  # Start an MLflow run
         try:
             clf = load_model('./models/model.pkl')
